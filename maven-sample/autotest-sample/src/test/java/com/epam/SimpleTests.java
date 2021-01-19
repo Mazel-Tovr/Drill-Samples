3f5d964c6f370b7fd1f370e5f459efd0b759e002
Roman <roman_ivanov3@epam.com>
2021-01-19 21:59:30 +0300

    add repository

---
+++ maven-sample/autotest-sample/src/test/java/com/epam/SimpleTests.java
@@ -0,0 +1,53 @@
+package com.epam;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit autotest example for petclinic app.
+ */
+public class SimpleTests {
+
+    private final String pathToServer = "http://localhost:8080";
+    private final CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getOwner4InfoPage() throws IOException {
+
+        HttpGet httpGet = new HttpGet(pathToServer + "/owners/4");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        assertEquals(response.getStatusLine().getStatusCode(), 200);
+    }
+
+
+    @Test
+    public void getOwner4EditPage() throws IOException {
+
+        HttpGet httpGet = new HttpGet(pathToServer + "/owners/4/edit");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        assertEquals(response.getStatusLine().getStatusCode(), 200);
+    }
+
+   // @Ignore
+    @Test
+    public void getHomePage() throws IOException {
+        HttpGet httpGet = new HttpGet(pathToServer + "/");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        assertEquals(response.getStatusLine().getStatusCode(), 200);
+    }
+
+}
